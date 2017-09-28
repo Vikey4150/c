aa2c5672607159df2155f37dff3fad5f190c971d
vikeyvsb <30316816+vikeyvsb@users.noreply.github.com>
2017-09-28 10:23:27 +0530

    Create Positive.C

---
+++ Positive.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+  if(a==0)
+    printf("zero");
+  elseif(a>0)
+    printf("positive");
+  else 
+    printf("negative");
+}
